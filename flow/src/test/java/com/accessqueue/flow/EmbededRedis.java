6794488dca6a5afae95c531ab36ef876d0e3e9b4
sohui.dev <113837605+qwerty00ui88@users.noreply.github.com>
2025-02-12 17:59:59 +0900

    test: UserQueueService 테스트 코드 추가

---
+++ flow/src/test/java/com/accessqueue/flow/EmbededRedis.java
@@ -0,0 +1,28 @@
+package com.accessqueue.flow;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.boot.test.context.TestConfiguration;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+
+
+@TestConfiguration
+public class EmbededRedis {
+    private final RedisServer redisServer;
+
+    public EmbededRedis() throws IOException {
+        this.redisServer = new RedisServer(63790);
+    }
+
+    @PostConstruct
+    public void start() throws IOException {
+        this.redisServer.start();
+    }
+
+    @PreDestroy
+    public void stop() throws IOException {
+        this.redisServer.stop();
+    }
+}
